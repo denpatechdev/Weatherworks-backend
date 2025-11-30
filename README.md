262afa89d1e752d5154d4b8d75f35bf528ed3247
denpatechdev <marinight17+denpatech@gmail.com>
2025-11-30 15:30:45 -0600

    license and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# About Weatherworks
+
+Weatherworks is a weather application with social features to help understand weather conditions from community-based insights provided by its users.
+
+# Credits
+[Abdul Rafea](https://github.com/Abdul-Rafea) - Frontend
+[denpatech](https://github.com/denpatechdev), [XTCooper](https://github.com/XTCooper11) - Backend
---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from api import views
+
+urlpatterns = [
+    path('comments/<str:area>/post/', views.create_comment, name='create_comment'),
+    path('comments/<str:area>/', views.get_comments, name='get_comments'),
+    path('comments/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
+    path('comments/<int:comment_id>/like/', views.like_comment, name='like_comment')
+]

fa788a871e0d38a6c9105b889a76f931b9780c71
denpatechdev <marinight17+denpatech@gmail.com>
2025-11-30 15:18:52 -0600

    basic comment and like functionality

--- api/views.py
+++ api/views.py
@@ -1,3 +1,89 @@
 from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from app.models import Comment, Like
+from api import tools
 
 # Create your views here.
+@api_view(['GET'])
+def get_comments(req, area: str) -> Response:
+    comments = Comment.objects.filter(area=area)
+    comments_arr = []
+    for c in comments:
+        comments_arr.append({
+            'id': c.pk,
+            'area': c.area,
+            'contents': c.contents,
+            'ip': c.ip,
+            'likes': Like.objects.filter(comment=c).count()
+        })
+    return Response({'comments': comments_arr})
+
+@api_view(['POST'])
+def create_comment(req, area: str) -> Response:
+    if area is None or len(area) < 1:
+        return Response({'message': 'Area field is empty or null'})
+    contents = req.data.get('contents')
+    if contents is None or len(contents) < 1:
+        return Response({'message': 'Contents field is empty or null.'})
+    ip = tools.get_ip(req)
+    comment = Comment.objects.create(area=area, contents=contents, ip=ip)
+    return Response({
+        'id': comment.pk,
+        'area': area,
+        'contents': contents,
+        'ip': ip,
+        'likes': 0
+    })
+
+@api_view(['DELETE'])
+def delete_comment(req, comment_id: int) -> Response:
+    user: User = req.user
+    if user.is_authenticated and (user.is_staff or user.is_superuser):
+        if comment := Comment.objects.filter(pk=comment_id):
+            deleted_comment_data = {}
+            deleted_comment_data['area'] = comment[0].area
+            deleted_comment_data['contents'] = comment[0].contents
+            deleted_comment_data['ip'] = comment[0].ip
+            comment[0].delete()
+            return Response(deleted_comment_data)
+        else:
+            return Response({'message': 'Comment not found'})
+    else:
+        return Response({'message': 'You do not have authorization to delete comments.'})
+
+@api_view(['POST', 'DELETE'])
+def like_comment(req, comment_id: int) -> Response:
+
+    ip = tools.get_ip(req)
+    comment = Comment.objects.get(pk=comment_id) if Comment.objects.filter(pk=comment_id).exists() else None
+    if req.method == 'POST':
+        if not comment:
+            return Response({'message': 'Comment not found.'})
+        if not Like.objects.filter(comment=comment, ip=ip).exists():
+            like = Like.objects.create(comment=comment, ip=ip)
+            return Response({
+                'id': like.pk,
+                'ip': ip,
+                'comment': comment.pk
+            })
+        else:
+            return Response({'message': 'Comment already liked.'})
+    elif req.method == 'DELETE':
+        if not comment:
+            return Response({'message': 'Comment not found.'})
+        if Like.objects.filter(comment=comment, ip=ip).exists():
+            like = Like.objects.get(comment=comment, ip=ip)
+            deleted_like_data = {
+                'id': like.pk,
+                'ip': like.ip,
+                'comment': comment.pk
+            }
+            Like.objects.get(comment=comment, ip=ip).delete()
+            return Response(deleted_like_data)
+        else:
+            return Response({'message': 'Comment already not liked.'})
+    
+    return Response()
--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class Comment(models.Model):
+    ip = models.GenericIPAddressField()
     area = models.CharField(max_length=256)
-    pinned = models.BooleanField(default=False)
     contents = models.CharField(max_length=500)
+
+class Like(models.Model):
+    ip = models.GenericIPAddressField()
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)

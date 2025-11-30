f875c35c01c8e509d12cbb93b8cba3dd28ae54ed
denpatechdev <marinight17+denpatech@gmail.com>
2025-11-30 13:50:55 -0600

    migrations + comments model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Comment(models.Model):
+    area = models.CharField(max_length=256)
+    pinned = models.BooleanField(default=False)
+    contents = models.CharField(max_length=500)
